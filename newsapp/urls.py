55e1884ea50d6f955c1c81d428a8e52d011b19a2
soso0024 <131151398+soso0024@users.noreply.github.com>
2024-09-29 11:19:54 +0900

    update: header

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("sports/", views.sports),
-    path("science/", views.science),
+    path("", views.index, name="index"),
+    path("sports/", views.sports, name="sports"),
+    path("science/", views.science, name="science"),
 ]
