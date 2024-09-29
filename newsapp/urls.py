47c9f844cdb26f5bb7ba475fd60cc1d8d03cf6c4
soso0024 <131151398+soso0024@users.noreply.github.com>
2024-09-29 10:32:53 +0900

    add: general contents and function to get news

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.index),
     path("sports/", views.sports),
     path("science/", views.science),
 ]
--- newsapp/views.py
+++ newsapp/views.py
@@ -9,10 +9,9 @@ env.read_env(".env")
 MEDIA_API_KEY = env.get_value("MEDIA_API_KEY", str)
 
 
-# Create your views here.
-def sports(request):
+def get_news(category):
     r = requests.get(
-        f"http://api.mediastack.com/v1/news?access_key={MEDIA_API_KEY}&categories=sports"
+        f"http://api.mediastack.com/v1/news?access_key={MEDIA_API_KEY}&categories={category}"
     )
     res = r.json()
     data = res["data"]
@@ -26,23 +25,20 @@ def sports(request):
         image.append(i["image"])
         url.append(i["url"])
     news = zip(title, description, image, url)
+    return news
+
+
+def index(request):
+    news = get_news("general")
+    return render(request, "newsapp/index.html", {"news": news})
+
+
+# Create your views here.
+def sports(request):
+    news = get_news("sports")
     return render(request, "newsapp/sports.html", {"news": news})
 
 
 def science(request):
-    r = requests.get(
-        f"http://api.mediastack.com/v1/news?access_key={MEDIA_API_KEY}&categories=science"
-    )
-    res = r.json()
-    data = res["data"]
-    title = []
-    description = []
-    image = []
-    url = []
-    for i in data:
-        title.append(i["title"])
-        description.append(i["description"])
-        image.append(i["image"])
-        url.append(i["url"])
-    news = zip(title, description, image, url)
+    news = get_news("science")
     return render(request, "newsapp/science.html", {"news": news})
